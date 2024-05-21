a86fd129a140551c711d5ca4d93d8b797ade33c4
Kağan ARTAN <kagan.artan@boun.edu.tr>
2024-05-22 01:15:24 +0300

    Add project files.

---
+++ CET322_HW1_Books/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CET322_HW1_Books.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace CET322_HW1_Books.Controllers
+{
+    public class BookController : Controller
+    {
+        private static List<Book> books = new List<Book>
+    {
+        new Book { Name = "Suç ve Ceza", Author = "Dostoyevski", PageNumber = 100, DueDate = DateTime.Now.AddDays(5) },  
+        new Book { Name = "Sefiller", Author = "Vitor Hugo", PageNumber = 150, DueDate = DateTime.Now },              
+        new Book { Name = "1984", Author = "George Orwell", PageNumber = 200, DueDate = DateTime.Now.AddDays(-5) } 
+    };
+
+        public IActionResult Detail(int id)
+        {
+            if (id < 0 || id >= books.Count)
+            {
+                return NotFound();
+            }
+
+            var book = books[id];
+            return View(book);
+        }
+    }
+
+}
---
+++ CET322_HW1_Books/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿namespace CET322_HW1_Books.Models
+{
+    public class Book
+    {
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public int PageNumber { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public string GetDueDateStatus()
+        {
+            int daysRemaining = (DueDate - DateTime.Now).Days;
+
+            if (daysRemaining > 0)
+                return "green";
+            else if (daysRemaining == 0)
+                return "yellow";
+            else
+                return "red";
+        }
+
+        public int DaysRemaining()
+        {
+            return (DueDate - DateTime.Now).Days;
+        }
+    }
+
+}
